be6f2a48e120e5c1ece67061f02bc6acad533e92
Kenny Tran <ktra99@outlook.com>
2023-02-07 00:01:12 +0100

    hide mobile nav on blog page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -122,7 +122,12 @@ export default function Layout({
         </div>
       </nav>
       {children}
-      <div className="fixed bottom-0 z-20 flex w-full items-center justify-center space-x-8 rounded-t-3xl bg-black bg-opacity-40 p-4 text-white backdrop-blur-sm xs:space-x-12 xs:p-8 sm:hidden">
+      <div
+        className={clsx(
+          route.includes("/posts/") && "hidden",
+          "fixed bottom-0 z-20 flex w-full items-center justify-center space-x-8 rounded-t-3xl bg-black bg-opacity-40 p-4 text-white backdrop-blur-sm xs:space-x-12 xs:p-8 sm:hidden"
+        )}
+      >
         <Link href="/">
           <HomeIcon
             className={clsx(
