fe5b9b6987bcfaaec9658ed9d56fab00a3250f65
Kenny Tran <kenny@telnect.com>
2023-03-04 17:07:31 +0100

    add github button to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import GitHubButton from "react-github-btn";
 import useLocale from "@src/hooks/useLocale";
 
 export function Page() {
@@ -5,10 +6,22 @@ export function Page() {
   return (
     <footer className="mx-auto my-4 px-4 sm:max-w-[33rem] sm:px-0 xl:max-w-7xl">
       <hr />
-      <mark className="mt-4 mb-24 block bg-transparent font-semibold text-white xs:mb-32 xs:text-lg sm:mt-6">
-        © {new Date().getFullYear() + " Kenny Tran."} {""}
-        {translate("All rights reserved.")}
-      </mark>
+      <div className="mt-4 mb-24 flex items-center justify-between xs:mb-32 sm:mt-6">
+        <mark className="block bg-transparent font-semibold text-white xs:text-lg">
+          © {new Date().getFullYear() + " Kenny Tran."} {""}
+          {translate("All rights reserved.")}
+        </mark>
+        <GitHubButton
+          href="https://github.com/ktra99/digital-garden"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star ktra99/digital-garden on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </div>
     </footer>
   );
 }
@@ -17,10 +30,22 @@ export function Post() {
   const translate = useLocale();
   return (
     <footer className="mx-auto mb-4 max-w-3xl px-4 font-semibold text-white xs:mt-4 xs:mb-16 xs:text-lg">
-      <mark className="bg-transparent text-white">
-        © {new Date().getFullYear() + " Kenny Tran."} {""}
-        {translate("All rights reserved.")}
-      </mark>
+      <div className="flex items-center justify-between">
+        <mark className="bg-transparent text-white">
+          © {new Date().getFullYear() + " Kenny Tran."} {""}
+          {translate("All rights reserved.")}
+        </mark>
+        <GitHubButton
+          href="https://github.com/ktra99/digital-garden"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star ktra99/digital-garden on GitHub"
+        >
+          Star
+        </GitHubButton>
+      </div>
     </footer>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -156,7 +156,7 @@ export default function Navbar({ posts }: { posts: PostMeta[] }) {
                           <div className="pt-3 pb-2">
                             <div className="flex flex-row-reverse items-center justify-between px-4">
                               <div className="-mr-2">
-                                <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500">
+                                <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset">
                                   <span className="sr-only">Close menu</span>
                                   <XMarkIcon
                                     className="h-6 w-6"
